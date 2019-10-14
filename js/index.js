83923962adfcf82000534a6d07ed7bf57dec4e82
Viktoriia Hasko <dev.viktoriia.hasko@gmail.com>
2019-10-14 16:54:25 +0300

    Add responsive

--- js/index.js
+++ js/index.js
@@ -1,26 +1,51 @@
-/* конфигурация */
-let list = carousel.querySelector('ul');
-let listElems = carousel.querySelectorAll('li');
+(function () {
+  // Config
+  let carousel = document.querySelector('#carousel');
+  let list = carousel.querySelector('ul');
+  let listElems = carousel.querySelectorAll('li');
+  let position = 0; // set initial position
+  let width = 0;
+  const elementMarginX = 30;
 
-listElems.forEach(element => {
-  element.style.width = (carousel.offsetWidth / 3) - 30 + 'px';
-});
+  const setCarouselElementWidth = () => {
+    const elementWidth = document.body.clientWidth <= 768
+    ? carousel.offsetWidth
+    : carousel.offsetWidth / 3;
 
-let width = carousel.querySelector('li').offsetWidth + 30;
-let position = 0; // положение ленты прокрутки
+    listElems.forEach(element => {
+      element.style.width = elementWidth - elementMarginX + 'px';
+    });
+  }
 
-carousel.querySelector('.next').onclick = () => {
-  // сдвиг вправо
-  position -= width;
-  // Повертаємось на початок, якщо останній елемент
-  (position === - width * (listElems.length - 2)) && (position = 0)
-  list.style.marginLeft = position + 'px';
-};
+  const getCarouselElementWidth = () => carousel.querySelector('li').offsetWidth + elementMarginX;
 
-carousel.querySelector('.prev').onclick = () => {
-  // сдвиг влево
-  position += width;
-  // Не дозволяє зсувати далі за перший елемент
-  position = Math.min(position, 0)
-  list.style.marginLeft = position + 'px';
-};
+  const moveToNextElement = () => {
+    position -= width;
+    // Return to the beginning, if this is the last element
+    (position === - width * (listElems.length - 2)) && (position = 0)
+    list.style.marginLeft = position + 'px';
+  }
+
+  const moveToPrevElement = () => {
+    position += width;
+    // Do not allow to move behind the first element
+    position = Math.min(position, 0)
+    list.style.marginLeft = position + 'px';
+  }
+
+  window.addEventListener('load', () => {
+    setCarouselElementWidth();
+
+    width = getCarouselElementWidth();
+  })
+
+  window.addEventListener('resize', () => {
+    setCarouselElementWidth();
+
+    width = getCarouselElementWidth();
+  })
+
+  carousel.querySelector('.next').addEventListener('click', moveToNextElement);
+  carousel.querySelector('.prev').addEventListener('click', moveToPrevElement);
+
+}());
